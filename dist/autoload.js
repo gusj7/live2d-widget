57b1c0f5c1d9ade4fa6e59f5f9b2a1e6d180a48d
gusj7 <56310145+gusj7@users.noreply.github.com>
2025-06-05 00:03:27 +0800

    autoload.js

--- dist/autoload.js
+++ dist/autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = 'https://cdn.jsdelivr.net/gh/gusj7/live2d-widget@4.0/dist/';
+const live2d_path = 'https://cdn.jsdelivr.net/gh/gusj7/live2d-widget@6.0/dist/';
 // const live2d_path = '/dist/';
 
 // 封装异步加载资源的方法
